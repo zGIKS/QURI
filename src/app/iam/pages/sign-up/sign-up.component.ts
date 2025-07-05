9256784ed5cf51ed6b98acba74cac4533e06e2fa
JonatanFD <u20211b995@upc.edu.pe>
2025-07-05 16:58:52 -0500

    feat(signup): Add role selection to sign-up form and update translations

--- src/app/iam/pages/sign-up/sign-up.component.ts
+++ src/app/iam/pages/sign-up/sign-up.component.ts
@@ -10,6 +10,9 @@ import {MatIcon} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
 import { LanguageSwitcherComponent } from '../../../shared/components/language-switcher.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
@@ -32,13 +35,18 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
     MatSuffix,
     RouterLink,
     LanguageSwitcherComponent,
-    TranslateModule
+    TranslateModule,
+    MatSelectModule,
+    MatOptionModule,
+    CommonModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent implements OnInit {
   form!: FormGroup;
+  roles: Array<{ label: string; value: string }> = [];
+  selectedRole = 'customer';
   submitted: boolean = false;
   hidePassword = true;
   hideConfirmPassword = true;
@@ -51,10 +59,15 @@ export class SignUpComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.roles = [
+      { label: this.translateService.instant('signup.roleField.customer'), value: 'customer' },
+      { label: this.translateService.instant('signup.roleField.manufacturer'), value: 'manufacturer' }
+    ];
     this.form = this.builder.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required]]
+      confirmPassword: ['', [Validators.required]],
+      role: ['customer', Validators.required]
     }, { validators: this.passwordMatchValidator });
   }
 
@@ -64,9 +77,14 @@ export class SignUpComponent implements OnInit {
     this.isLoading = true;
     this.submitted = true;
 
-    let username = this.form.value.username;
-    let password = this.form.value.password;
-    const signUpRequest = new SignUpRequest(username, password, ['ROLE_USER']);
+    const username = this.form.value.username;
+    const password = this.form.value.password;
+    const role = this.form.value.role;
+    let roles = ['ROLE_USER'];
+    if (role === 'manufacturer') {
+      roles.push('ROLE_MANUFACTURER');
+    }
+    const signUpRequest = new SignUpRequest(username, password, roles);
 
     this.authenticationService.signUp(signUpRequest);
 
